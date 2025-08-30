c0802b3088873224e7a84dca3d28bfacfa6e798c
Mubeen Hussain <mubeenhussain8@gmail.com>
2025-08-31 00:50:22 +0500

    ==

--- app/layout.js
+++ app/layout.js
@@ -5,18 +5,11 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "../i18n/i18n";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-
-// Components
 import Navbar from "./components/Navbar";
 import AuthNavbar from "./components/AuthNavbar";
 import Footer from "./components/Footer";
 import { PopupProvider } from "./contexts/PopupContext";
 import { Providers } from './providers';
-
-export const metadata = {
-  title: "Portal",
-  description: "Next.js project",
-};
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
@@ -31,7 +24,6 @@ export default function RootLayout({ children }) {
       <body>
         <Providers>
           <I18nextProvider i18n={i18n}>
-            {/* ✅ Wrap everything inside PopupProvider */}
             <PopupProvider>
               {isAuthRoute ? <AuthNavbar /> : <Navbar />}
               <main>{children}</main>
