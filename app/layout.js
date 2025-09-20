c569452c7f7e8c95b7b1e345bb9234a241d494bf
Mubeen Hussain <mubeenhussain8@gmail.com>
2025-09-20 19:50:48 +0500

    fix

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
     return (
       <html lang="en">
         <body>
-          <Loading />
+          {/* <Loading /> */}
         </body>
       </html>
     );
