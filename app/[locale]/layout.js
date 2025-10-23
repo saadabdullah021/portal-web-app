ed335b77f357a3d1ec650f9f6d3687a1492ce925
Faran Javed <faran.code@gmail.com>
2025-10-24 01:33:28 +0500

    move to locale

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -2,13 +2,13 @@
 
 import "./globals.css";
 import { I18nextProvider } from "react-i18next";
-import i18n from "../i18n/i18n";
+import i18n from "../../../i18n/i18n";
 import { useEffect, useState } from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import { PopupProvider } from "./contexts/PopupContext";
-import { Providers } from './providers';
-import Loading from './loading';
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import { PopupProvider } from "../../contexts/PopupContext";
+import { Providers } from "../../providers";
+import Loading from "../../loading";
 
 export default function RootLayout({ children }) {
   const [isAuthenticated, setIsAuthenticated] = useState(true);
@@ -17,24 +17,22 @@ export default function RootLayout({ children }) {
   useEffect(() => {
     const loggedIn = localStorage.getItem("isLoggedIn") === "true";
     setIsAuthenticated(loggedIn);
-    
+
     const savedLanguage = localStorage.getItem("i18nextLng") || "en";
     document.documentElement.dir = savedLanguage === "ar" ? "rtl" : "ltr";
     document.documentElement.lang = savedLanguage;
-    
+
     if (i18n.language !== savedLanguage) {
       i18n.changeLanguage(savedLanguage);
     }
-    
+
     setIsHydrated(true);
   }, []);
 
   if (!isHydrated) {
     return (
       <html lang="en">
-        <body>
-          {/* <Loading /> */}
-        </body>
+        <body>{/* <Loading /> */}</body>
       </html>
     );
   }
--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,18 +1,13 @@
-
 import HomeContent from "./components/HomeContent";
 
 export const metadata = {
   title: "PORTAL | Book Luxury Chalets | Reserve Your Perfect Getaway Today",
-
 };
 
 export default function Home() {
   return (
-
-  <div className=" px-4 ">
- <HomeContent/>
-  
-  </div>
+    <div className=" px-4 ">
+      <HomeContent />
+    </div>
   );
 }
-
